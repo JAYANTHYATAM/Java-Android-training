341567bea3bfbd1ff5ef2a0a137403609e3e6404
JAYANTHYATAM <113885757+JAYANTHYATAM@users.noreply.github.com>
2023-01-02 10:48:06 +0530

    Add files via upload

---
+++ Interfaces.java
@@ -0,0 +1,20 @@
+interface A 
+{
+	Void A display();
+}
+
+Class AB implements A
+{
+	Public void Adisplay()
+{
+	System.out.println("A DISPLAY");
+}
+}
+
+Class A Main{
+	Public static void main (String args());
+{
+	AB obj = new AB();
+	OBJ.Adisplay():
+}
+}
